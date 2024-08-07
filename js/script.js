9136615250909158b87f52f7b3e42ca724366b08
ninjoix <ninjoix@gmail.com>
2024-08-07 12:51:39 +0300

    I've added a slider and am starting to design the 'FAQ' block.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: true,        
+  },
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  
+});
