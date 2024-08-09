1b782f9c3f50d4d1e81fb693044a780bcb531e4f
ninjoix <ninjoix@gmail.com>
2024-08-09 17:06:58 +0300

    Finished the slider, make a 'community' block

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 var swiper = new Swiper(".mySwiper", {
-  autoplay: {
-    delay: 1500,
-    disableOnInteraction: true,        
-  },
   slidesPerView: 4,
   spaceBetween: 30,
-  centeredSlides: true,
-  
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleHeaders = document.querySelectorAll('.toggle-header');
+
+  toggleHeaders.forEach(header => {
+      header.addEventListener('click', function() {
+          const content = this.nextElementSibling;
+          const icon = this.querySelector('.toggle-icon');
+
+          if (content.classList.contains('show')) {
+              content.classList.remove('show');
+              icon.textContent = '+';
+          } else {
+              content.classList.add('show');
+              icon.textContent = '×';
+          }
+      });
+  });
 });
